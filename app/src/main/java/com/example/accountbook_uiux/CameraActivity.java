fe4bd9c315f8dbdadc04eb84054b5a7edac8527a
dlgkdud95 <lhyy0195@gmail.com>
2021-10-14 04:18:31 +0900

    구글 cloud vision OCR 연동

--- app/src/main/java/com/example/accountbook_uiux/CameraActivity.java
+++ app/src/main/java/com/example/accountbook_uiux/CameraActivity.java
@@ -1,102 +1,357 @@
 package com.example.accountbook_uiux;
 
+import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequest;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class CameraActivity extends AppCompatActivity {
 
-    ImageView camera_receipt;
-    Button buttonSelected, buttonCamera;
-    //카메라 - 파일
-    File file;
-    private Uri uri; //file, 웹 주소 관련 처리하는 객체
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyBAsxXiRCTcKng2GqeGeEP5G9SRKHLOE7U";
+    public static final String FILE_NAME = "temp.jpg";
+    private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
+    private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
+    private static final int MAX_LABEL_RESULTS = 10;
+    private static final int MAX_DIMENSION = 1200;
+
+    private static final String TAG = CameraActivity.class.getSimpleName();
+    private static final int GALLERY_PERMISSIONS_REQUEST = 0;
+    private static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
+    private TextView receiptText;
+    private ImageView receiptImage;
+
+    //fab_main 버튼의 상태, 기본값 -> 선택하지 않은 상태
+    private boolean isFabOpen = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fab_receiptview);
+        setContentView(R.layout.receiptscan_view);
 
-        camera_receipt = findViewById(R.id.camera_receipt);
-        buttonSelected = findViewById(R.id.buttonSelected);
-        buttonCamera = findViewById(R.id.buttonCamera);
+        FloatingActionButton fab_receiptSelect = findViewById(R.id.fab_receiptSelect);
+        FloatingActionButton fab_camera = findViewById(R.id.fab_camera);
+        FloatingActionButton fab_album = findViewById(R.id.fab_album);
 
-        buttonCamera.setOnClickListener(new View.OnClickListener() {    //촬영 버튼 누르면 카메라 실행됨
-            @Override                                                   //이 버튼 말고 메인화면에서 카메라버튼(fab)누르면 바로 카메라 촬영 후 사진이 보이게 하고 싶은데 카메라 화면에서 넘어가질 않음. 나중에 수정
-            public void onClick(View view) {
-                takePicture();
-            }
-        });
-
-        buttonSelected.setOnClickListener(new View.OnClickListener() {
+        //(플로팅액션버튼)버튼을 누르면 화면이 넘어감(fab_main 제외)
+        fab_receiptSelect.setOnClickListener(new View.OnClickListener() { //버튼 클릭시 수행할 동작 지정
             @Override
             public void onClick(View view) {
-                Intent selectIntent = new Intent(getApplicationContext(), MoneyInputActivity.class); //buttonSelected(확인)을 누르면 수입, 지출입력 화면으로 이동
-                startActivity(selectIntent);
+                toggleFab();
             }
         });
-    }
-
-    //카메라 연동 매소드
-    public void takePicture() {
-        try {
-            file = createFile(); //사진 찍고 결과물 저장할 파일 만듦
-            if (file.exists()) {
-                file.delete(); //파일이 존재한다면 삭제
+        fab_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCamera(); //카메라로 넘어감
             }
+        });
+        fab_album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGalleryChooser();  //앨범으로 넘어감
+            }
+        });
 
-            file.createNewFile();
-        } catch (Exception e) {
-            e.printStackTrace(); //에러 발생 경로 찍어줌
+        Button bt_register = findViewById(R.id.bt_register);
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MoneyInputActivity.class);   //확인 버튼을 누르면 MoneyInputActivity 클래스로 넘어감
+                startActivity(intent);
+            }
+        });
+
+        receiptText = findViewById(R.id.receiptText);
+        receiptImage = findViewById(R.id.receiptImage);
+    }
+    //fab 움직이게 하는 메소드
+    public void toggleFab() {
+        if (isFabOpen) { //닫힌상태
+            //writing, camera y축으로 0만큼 움직임
+            ObjectAnimator.ofFloat(findViewById(R.id.fab_album), "translationY", 0f).start();
+            ObjectAnimator.ofFloat(findViewById(R.id.fab_camera), "translationY", 0f).start();
+        } else {
+            //writing -100, camera -200 만큼 y축으로움직임
+            ObjectAnimator.ofFloat(findViewById(R.id.fab_album), "translationY", -150f).start();
+            ObjectAnimator.ofFloat(findViewById(R.id.fab_camera), "translationY", -290f).start();
         }
 
-        //file객체로부터 Uri 객체 만들기
-        if (Build.VERSION.SDK_INT >= 24) { // API 레벨이  24이상일 때
-            uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, file); //file에 대해 uri 생성
-        }                                                                                    //카메라 앱에서 공유하여 쓸 수 있는 파일 정보를 uri객체로 만듦
-        else {
-            uri = Uri.fromFile(file);
+        isFabOpen = !isFabOpen;
+    }
+
+    public void startGalleryChooser() {
+        if (PermissionUtils.requestPermission(this, GALLERY_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                    GALLERY_IMAGE_REQUEST);
         }
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //내장 되어 있는 카메라 기능을 intent로 선언, 카메라 앱 실행
-        cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); //기존 frag에 새 frag추가, 종료 할 때까지 URI 접근에 대한 허가를 받음
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri); //FileProvider.getUriForFile에서 받은 uri객체, EXTRA_OUTPUT키를 사용해 인텐트에 부가 데이터로 추가
-        startActivityForResult(cameraIntent, 101); //intent(카메라 앱 실행)에서 받은 request 값 시스템으로 전달
-
     }
 
-    private File createFile() {
-        String filename = "capture.jpg";
-        File outFile = new File(getFilesDir(), filename); //저장 경로 반환, 새로운 파일 만들고 저장
-        Log.d("Main", "File path : " + outFile.getAbsolutePath());  //입력된 절대경로 그대로 표현
-
-        return outFile;
+    public void startCamera() {
+        if (PermissionUtils.requestPermission(
+                this,
+                CAMERA_PERMISSIONS_REQUEST,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", getCameraFile());
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+        }
     }
 
-    //카메라 앱 종료 후 다시 돌아올 때 호출되는 메소드, 카메라로 찍은 사진 파일에서 확인 할 수 있음
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public File getCameraFile() {
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(dir, FILE_NAME);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 101 && resultCode == RESULT_OK) {   //101은 takePicture()메소드에서 받은 값
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 5;           //해상도가 높은 경우 bitmap객체의 크기도 커져서 임의로 비율을 1/8크기로 축소
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options); //이미지 파일을 Bitmap객체로 만듦, Bitmap객체는 메모리에 만들어지는 이미지
-
-            camera_receipt.setImageBitmap(bitmap); ////Bitmap객체로 만들어진 파일이 이미지뷰로 보임
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            uploadImage(data.getData());
+        } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", getCameraFile());
+            uploadImage(photoUri);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+                    startCamera();
+                }
+                break;
+            case GALLERY_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, GALLERY_PERMISSIONS_REQUEST, grantResults)) {
+                    startGalleryChooser();
+                }
+                break;
+        }
+    }
+
+    public void uploadImage(Uri uri) {
+        if (uri != null) {
+            try {
+                // scale the image to save on bandwidth
+                Bitmap bitmap =
+                        scaleBitmapDown(
+                                MediaStore.Images.Media.getBitmap(getContentResolver(), uri),
+                                MAX_DIMENSION);
+
+                callCloudVision(bitmap);
+                receiptImage.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                Log.d(TAG, "Image picking failed because " + e.getMessage());
+                Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Log.d(TAG, "Image picker gave us a null image.");
+            Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private Vision.Images.Annotate prepareAnnotationRequest(Bitmap bitmap) throws IOException {
+        HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        VisionRequestInitializer requestInitializer =
+                new VisionRequestInitializer(CLOUD_VISION_API_KEY) {
+                    /**
+                     * We override this so we can inject important identifying fields into the HTTP
+                     * headers. This enables use of a restricted cloud platform API key.
+                     */
+                    @Override
+                    protected void initializeVisionRequest(VisionRequest<?> visionRequest)
+                            throws IOException {
+                        super.initializeVisionRequest(visionRequest);
+
+                        String packageName = getPackageName();
+                        visionRequest.getRequestHeaders().set(ANDROID_PACKAGE_HEADER, packageName);
+
+                        String sig = PackageManagerUtils.getSignature(getPackageManager(), packageName);
+
+                        visionRequest.getRequestHeaders().set(ANDROID_CERT_HEADER, sig);
+                    }
+                };
+
+        Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+        builder.setVisionRequestInitializer(requestInitializer);
+
+        Vision vision = builder.build();
+
+        BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                new BatchAnnotateImagesRequest();
+        batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
+            AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+
+            // Add the image
+            Image base64EncodedImage = new Image();
+            // Convert the bitmap to a JPEG
+            // Just in case it's a format that Android understands but Cloud Vision
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+            // Base64 encode the JPEG
+            base64EncodedImage.encodeContent(imageBytes);
+            annotateImageRequest.setImage(base64EncodedImage);
+
+            // add the features we want
+            annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
+                Feature labelDetection = new Feature();
+                labelDetection.setType("TEXT_DETECTION");
+                labelDetection.setMaxResults(MAX_LABEL_RESULTS);
+                add(labelDetection);
+            }});
+
+            // Add the list of one thing to the request
+            add(annotateImageRequest);
+        }});
+
+        Vision.Images.Annotate annotateRequest =
+                vision.images().annotate(batchAnnotateImagesRequest);
+        // Due to a bug: requests to Vision API containing large images fail when GZipped.
+        annotateRequest.setDisableGZipContent(true);
+        Log.d(TAG, "created Cloud Vision request object, sending request");
+
+        return annotateRequest;
+    }
+
+    private static class LableDetectionTask extends AsyncTask<Object, Void, String> {
+        private final WeakReference<CameraActivity> mActivityWeakReference;
+        private Vision.Images.Annotate mRequest;
+
+        LableDetectionTask(CameraActivity activity, Vision.Images.Annotate annotate) {
+            mActivityWeakReference = new WeakReference<>(activity);
+            mRequest = annotate;
+        }
+
+        @Override
+        protected String doInBackground(Object... params) {
+            try {
+                Log.d(TAG, "created Cloud Vision request object, sending request");
+                BatchAnnotateImagesResponse response = mRequest.execute();
+                return convertResponseToString(response);
+
+            } catch (GoogleJsonResponseException e) {
+                Log.d(TAG, "failed to make API request because " + e.getContent());
+            } catch (IOException e) {
+                Log.d(TAG, "failed to make API request because of other IOException " +
+                        e.getMessage());
+            }
+            return "Cloud Vision API request failed. Check logs for details.";
+        }
+
+        protected void onPostExecute(String result) {
+            CameraActivity activity = mActivityWeakReference.get();
+            if (activity != null && !activity.isFinishing()) {
+                TextView imageDetail = activity.findViewById(R.id.receiptText);
+                imageDetail.setText(result);
+            }
+        }
+    }
+
+    private void callCloudVision(final Bitmap bitmap) {
+        // Switch text to loading
+        receiptText.setText(R.string.loading_message);
+
+        // Do the real work in an async task, because we need to use the network anyway
+        try {
+            AsyncTask<Object, Void, String> labelDetectionTask = new LableDetectionTask(this, prepareAnnotationRequest(bitmap));
+            labelDetectionTask.execute();
+        } catch (IOException e) {
+            Log.d(TAG, "failed to make API request because of other IOException " +
+                    e.getMessage());
+        }
+    }
+
+    private Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+    private static String convertResponseToString(BatchAnnotateImagesResponse response) {
+        StringBuilder message = new StringBuilder("I found these things:\n\n");
+
+        List<EntityAnnotation> labels = response.getResponses().get(0).getTextAnnotations();
+        if (labels != null) {
+            for (EntityAnnotation label : labels) {
+                message.append(String.format(Locale.US, "%.3f: %s", label.getScore(), label.getDescription()));
+                message.append("\n");
+            }
+        } else {
+            message.append("nothing");
+        }
+
+        return message.toString();
     }
 }
